3900487d01bb7cc7e04070a5435ec394ec4ba347
Orçun Çolak <orcun.colak@hazelcast.com>
2024-11-13 11:11:55 +0300

    vaultemplate example

--- src/main/java/com/colak/springtutorial/service/VaultService.java
+++ src/main/java/com/colak/springtutorial/service/VaultService.java
@@ -3,6 +3,8 @@ package com.colak.springtutorial.service;
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
+import org.springframework.vault.core.VaultKeyValueOperations;
+import org.springframework.vault.core.VaultKeyValueOperationsSupport;
 import org.springframework.vault.core.VaultTemplate;
 import org.springframework.vault.support.VaultResponse;
 
@@ -17,17 +19,31 @@ public class VaultService {
 
     private final VaultTemplate vaultTemplate;
 
-    public String getAge(){
-        return String.format("I am %s years old",age);
+    public String getAge() {
+        return String.format("I am %s years old", age);
     }
 
     public void writeSecret(String path, Map<String, Object> secrets) {
         vaultTemplate.write(path, secrets);
     }
 
+    // Another way of writing
+    public void writeSecret2(String path, Map<String, String> secrets) {
+        VaultKeyValueOperations operations = vaultTemplate.opsForKeyValue(path,
+                VaultKeyValueOperationsSupport.KeyValueBackend.KV_2);
+        operations.put(path, secrets);
+    }
+
     public Map<String, Object> readSecret(String path) {
         VaultResponse response = vaultTemplate.read(path);
         return response != null ? response.getData() : null;
+    }
+
+    // Another way of reading
+    public Map<String, Object> readSecret2(String path) {
+        VaultResponse response = vaultTemplate.opsForKeyValue(path,
+                VaultKeyValueOperationsSupport.KeyValueBackend.KV_2).get(path);
+        return response.getData();
     }
 
 }
